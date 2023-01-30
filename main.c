4996e1ce2f3d9240448e0dac09645a0168550c24
Etem Keskin <etmkeskin@gmail.com>
2023-01-30 16:09:15 -0500

    C test

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    char characterName[] = "John";
+    int age = 35;
+    printf("There is a man named %s,\n", characterName);
+    printf("he was %d years old.\n", age);
+
+    /*int age1 = 40;
+    double gpa = 3.7;
+    char grade = 'A';
+    char phrase[] = "at cuku"; /*Technically an array */
+
+    printf("My favourite %s is %d.\n","number", 7);
+    printf("%f\n", 5 + 4.5);
+    printf("%f\n", pow(2, 8)); /*Need to be %f format*/\
+    printf("%f\n", ceil(45.43));
+    printf("%f\n", floor(45.43));
+
+
+    return 0;
+}
